47b3dc064b579111d308cb2925e17445a6bbaf68
Jarret <Jarret@KODER-JARRET>
2016-11-17 10:29:24 -0800

    Add project files.

---
+++ ConsoleApplication2.Tests/UnitTest1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication2;
+
+namespace ConsoleApplication2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Cron CronFunctions;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            CronFunctions = new Cron();
+        }
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestBlankEveryWeek()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression( new DayOfWeek[] 
+            { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday }
+            );
+            Assert.AreEqual("0 0 0 ? * 5,3,1,6", result);
+        }
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleDaysEveryWeekStartTime()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression(new DayOfWeek[]
+            { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday },
+            new TimeSpan(9, 30, 0)
+            );
+            Assert.AreEqual("0 30 9 ? * 5,3,1,6", result);
+        }
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleDaysEveryWeekHourly()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression(new DayOfWeek[]
+            { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday },
+            frequency: 8
+            );
+            Assert.AreEqual("0 0 0/8 ? * 5,3,1,6", result);
+        }
+
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleDaysEveryWeekEndTime()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression(
+            new DayOfWeek[] { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday },
+            EndTime: new TimeSpan(15, 0, 0)
+            );
+            Assert.AreEqual("0 0 0-15 ? * 5,3,1,6", result);
+        }
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleDaysEveryWeekHourlyAndStartTime()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression(new DayOfWeek[]
+            { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday },
+            new TimeSpan(9, 30, 0),
+            8
+            );
+            Assert.AreEqual("0 30 9/8 ? * 5,3,1,6", result);
+        }
+
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleDaysEveryWeekStartTimeAndEndTime()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression(
+            new DayOfWeek[] { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday },
+            new TimeSpan(5,0,0),
+            EndTime: new TimeSpan(15, 0, 0)
+            );
+            Assert.AreEqual("0 0 5-15 ? * 5,3,1,6", result);
+        }
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleDaysEveryWeekRepeatHourlyAndEndTime()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression(
+            new DayOfWeek[] { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday },
+            frequency: 2,
+            EndTime: new TimeSpan(15, 0, 0)
+            );
+            Assert.AreEqual("0 0 0-15/2 ? * 5,3,1,6", result);
+        }
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleDaysEveryWeekRepeatHourlyAndStartTimeAndEndTime()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression(
+            new DayOfWeek[] { DayOfWeek.Friday, DayOfWeek.Wednesday, DayOfWeek.Monday, DayOfWeek.Saturday },
+            new TimeSpan(2,45,15),
+            2,
+            new TimeSpan(15, 0, 0)
+            );
+            Assert.AreEqual("15 45 2-15/2 ? * 5,3,1,6", result);
+        }
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleEveryDayEveryWeek()
+        {
+            string result = CronFunctions.GenerateWeeklyExpression( new DayOfWeek[] 
+            { DayOfWeek.Sunday, DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday, DayOfWeek.Saturday },
+            new TimeSpan(9, 30, 0));
+            Assert.AreEqual("0 30 9 ? * *", result);
+        }
+
+        [TestMethod]
+        [TestCategory("CRON")]
+        public void TestScheduleEveryDay()
+        {
+            string result = CronFunctions.GenerateDailyExpression(new TimeSpan(9, 30, 0));
+            Assert.AreEqual("0 30 9 ? * *", result);
+        }
+    }
+}
---
+++ ConsoleApplication2/Cron.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Cron
+    {
+        public string GenerateWeeklyExpression(DayOfWeek[] days, TimeSpan StartTime = default(TimeSpan), int frequency = 0, TimeSpan EndTime = default(TimeSpan))
+        {
+
+            //FORMAT
+            //SECONDS || MINUTES ||   HOURS  || DAYS OF THE MONTH || MONTH || DAYS OF THE WEEK || (optional) YEAR
+            //   0    ||    0    ||  18-22/2  ||        ?          ||   *   || MON,TUE,WED,THUR,FRI (or 1,2,3,4,5) || (nothing)
+            //Example Above = every 2 hours starting from 6pm to 10pm Monday - Friday
+
+
+
+            //Check if the task should just be considerred daily
+            if (days.Length> 6 )
+            {
+                return GenerateDailyExpression(StartTime);
+            }
+            string DaysToSchedule = "";
+            //schedule Seconds
+            DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length,  StartTime.Seconds + " ");
+            //schedule Minutes
+            DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, StartTime.Minutes + " ");
+
+            // schedule Hours
+            if(EndTime !=  default(TimeSpan) && frequency != 0)
+                //if both end time and frequncy are specified we need both '-' and '/' characters
+                DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, StartTime.Hours + "-" + EndTime.Hours + "/" + frequency.ToString() + " ");
+            else if (EndTime != default(TimeSpan))
+            {
+                //we only need '-' character
+                DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, StartTime.Hours + "-" + EndTime.Hours + " ");
+            }
+            else if(frequency != 0)
+            {
+                //we only need '/' character
+                DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, StartTime.Hours + "/" + frequency + " ");
+            }
+            else
+                //just need hours
+                DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, StartTime.Hours + " ");
+
+            //we don't need to worry about day of the month for a Weekly schedule
+            DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, "? ");
+            //we don't need to worry about months for a Weekly schedule
+            DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, "* ");
+
+            foreach (int day in days)
+            {
+                DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, day.ToString() + ",");
+            }
+
+            DaysToSchedule = DaysToSchedule.Remove(DaysToSchedule.Length-1);
+            //DaysToSchedule = DaysToSchedule.Insert(DaysToSchedule.Length, " ");
+            return DaysToSchedule;
+
+        }
+
+        public string GenerateDailyExpression(TimeSpan TimeOfDay = default(TimeSpan))
+        {
+            string schedule = "";
+            schedule = schedule.Insert(schedule.Length, TimeOfDay.Seconds + " ");
+            schedule = schedule.Insert(schedule.Length, TimeOfDay.Minutes + " ");
+            schedule = schedule.Insert(schedule.Length, TimeOfDay.Hours + " ");
+            schedule = schedule.Insert(schedule.Length, "? ");
+            schedule = schedule.Insert(schedule.Length, "* ");
+
+            schedule = schedule.Insert(schedule.Length, "*");
+            return schedule;
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
